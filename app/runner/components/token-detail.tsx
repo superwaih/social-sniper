00beb02039dfe13e28875b80eb981d3334a0e00e
Adewale shittu <sheriffhardewale@gmail.com>
2025-05-26 00:30:29 +0100

    Refactor TokenDetail component layout and enhance styling for better visual presentation

--- app/runner/components/token-detail.tsx
+++ app/runner/components/token-detail.tsx
@@ -44,147 +44,181 @@ export const TokenDetail: FC<KekimaxDialogProps> = ({ open, setOpen }) => {
         }}
         className="max-w-[623px] backdrop-blur-lg bg-[#020C12]/60 overflow-hidden text-white rounded-[20px]  p-8"
       >
-        <div className="bg-[#0A141A]/60 p-3 rounded-[12px]">
-          <DialogHeader>
-            <DialogTitle className="text-[#FFFFFFD4] font-mono text-lg border-b py-3 border-brandgray">
-              {dummyTokenData.name}{" "}
-              <span className="">({dummyTokenData.symbol})</span>
-            </DialogTitle>
-          </DialogHeader>
-          <div
-            className=" w-full my-2  backdrop-blur-lg 
-        grid grid-cols-3 gap-4  text-sm"
-          >
-            {tokenInfoData.map((token) => (
-              <div
-                key={token.name}
-                className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]"
+        <section className="relative flex flex-col gap-4">
+          {/* Background Glow SVG */}
+          <div className="pointer-events-none absolute inset-0 z-0">
+            <div className="absolute -top-32 -right-32 bottom-0">
+              <svg
+                width="367"
+                height="502"
+                viewBox="0 0 367 502"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                  {token.name}
-                </span>
-                <p className="font-mono text-[#FFFFFF5C] text-md break-all">
-                  {token.value}
-                </p>
-              </div>
-            ))}
-            <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
-              <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                RUG CHECK
-              </span>
-              <div className="bg-[#FF000021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FF0000] p-2 rounded-[10px]">
-                <Icons.warningIcon className="size-[20px] text-[#FF0000] " />
-                <p className="text-[#FFFFFF73]">Warning</p>
-              </div>
-            </div>
-            <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
-              <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                BUY/SELL RATIO:
-              </span>
-              <div className="font-mono justify-between flex items-center  text-[13px] w-full  text-[#FFFFFF5C] text-md ">
-                <p className="text-green-400 w-full">20% BUYS / 80% SELLS</p>
-              </div>
-            </div>
-            <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
-              <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                BUY/SELL RATIO:
-              </span>
-              <div className="bg-[#FFBA0021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FFBA00] p-2 rounded-[10px]">
-                <Icons.warningIcon className="size-[20px] text-[#FFBA00] " />
-                Warning
-              </div>
+                <circle
+                  opacity="0.13"
+                  cx="273"
+                  cy="229"
+                  r="273"
+                  fill="url(#glowGradient)"
+                />
+                <defs>
+                  <radialGradient
+                    id="glowGradient"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(273 229) rotate(90) scale(273)"
+                  >
+                    <stop stopColor="#FF4C02" />
+                    <stop offset="1" stopColor="#FF4C02" stopOpacity="0" />
+                  </radialGradient>
+                </defs>
+              </svg>
             </div>
           </div>
-          <div className="h-[1px] w-full bg-brandgray my-3 "></div>
-          <div className="flex justify-between items-center">
-            <div className="flex font-mono text-sm text-white  gap-2">
-              <ChevronDown />
-              <p>VIEW DETAILS</p>
-            </div>
-            <div className="flex gap-4 ">
-              <button className="flex gap-3 items-center border p-2 rounded-md border-[#779CBF6B]">
-                <Icons.trackicon />
-                TRACK TOKEN
-              </button>
-              <button className="flex bg-[#FF4C02] text-[#FFFFFFD4] gap-3 items-center border p-2 rounded-md border-[#779CBF6B]">
-                <Icons.autobuy />
-                SET AUTO BUY
-              </button>
-            </div>
-          </div>
-        </div>
 
-        <div className="bg-[#0A141A]/60 p-3 rounded-[12px]">
-          <DialogHeader>
-            <DialogTitle className="text-[#FFFFFFD4] font-mono text-lg border-b py-3 border-brandgray">
-              {dummyTokenData.name}{" "}
-              <span className="">({dummyTokenData.symbol})</span>
-            </DialogTitle>
-          </DialogHeader>
-          <div
-            className=" w-full my-2  backdrop-blur-lg 
+          <div className="bg-[#0A141A]/60 p-3 rounded-[12px]">
+            <DialogHeader>
+              <DialogTitle className="text-[#FFFFFFD4] font-mono text-lg border-b py-3 border-brandgray">
+                {dummyTokenData.name}{" "}
+                <span className="">({dummyTokenData.symbol})</span>
+              </DialogTitle>
+            </DialogHeader>
+            <div
+              className=" w-full my-2  backdrop-blur-lg 
         grid grid-cols-3 gap-4  text-sm"
-          >
-            {tokenInfoData.map((token) => (
-              <div
-                key={token.name}
-                className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]"
-              >
+            >
+              {tokenInfoData.map((token) => (
+                <div
+                  key={token.name}
+                  className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]"
+                >
+                  <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                    {token.name}
+                  </span>
+                  <p className="font-mono text-[#FFFFFF5C] text-md break-all">
+                    {token.value}
+                  </p>
+                </div>
+              ))}
+              <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
                 <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                  {token.name}
+                  RUG CHECK
                 </span>
-                <p className="font-mono text-[#FFFFFF5C] text-md break-all">
-                  {token.value}
-                </p>
+                <div className="bg-[#FF000021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FF0000] p-2 rounded-[10px]">
+                  <Icons.warningIcon className="size-[20px] text-[#FF0000] " />
+                  <p className="text-[#FFFFFF73]">Warning</p>
+                </div>
               </div>
-            ))}
-            <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
-              <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                RUG CHECK
-              </span>
-              <div className="bg-[#FF000021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FF0000] p-2 rounded-[10px]">
-                <Icons.warningIcon className="size-[20px] text-[#FF0000] " />
-                <p className="text-[#FFFFFF73]">Warning</p>
+              <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
+                <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                  BUY/SELL RATIO:
+                </span>
+                <div className="font-mono justify-between flex items-center  text-[13px] w-full  text-[#FFFFFF5C] text-md ">
+                  <p className="text-green-400 w-full">20% BUYS / 80% SELLS</p>
+                </div>
+              </div>
+              <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
+                <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                  BUY/SELL RATIO:
+                </span>
+                <div className="bg-[#FFBA0021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FFBA00] p-2 rounded-[10px]">
+                  <Icons.warningIcon className="size-[20px] text-[#FFBA00] " />
+                  Warning
+                </div>
               </div>
             </div>
-            <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
-              <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                BUY/SELL RATIO:
-              </span>
-              <div className="font-mono justify-between flex items-center  text-[13px] w-full  text-[#FFFFFF5C] text-md ">
-                <p className="text-green-400 w-full">20% BUYS / 80% SELLS</p>
+            <div className="h-[1px] w-full bg-brandgray my-3 "></div>
+            <div className="flex justify-between items-center">
+              <div className="flex font-mono text-sm text-white  gap-2">
+                <ChevronDown />
+                <p>VIEW DETAILS</p>
               </div>
-            </div>
-            <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
-              <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
-                BUY/SELL RATIO:
-              </span>
-              <div className="bg-[#FFBA0021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FFBA00] p-2 rounded-[10px]">
-                <Icons.warningIcon className="size-[20px] text-[#FFBA00] " />
-                Warning
+              <div className="flex gap-4 ">
+                <button className="flex gap-3 items-center border p-2 rounded-md border-[#779CBF6B]">
+                  <Icons.trackicon />
+                  TRACK TOKEN
+                </button>
+                <button className="flex bg-[#FF4C02] text-[#FFFFFFD4] gap-3 items-center border p-2 rounded-md border-[#779CBF6B]">
+                  <Icons.autobuy />
+                  SET AUTO BUY
+                </button>
               </div>
             </div>
           </div>
-          <div className="h-[1px] w-full bg-brandgray my-3 "></div>
-          <div className="flex justify-between items-center">
-            <div className="flex font-mono text-sm text-white  gap-2">
-              <ChevronDown />
-              <p>VIEW DETAILS</p>
-            </div>
-            <div className="flex gap-4 ">
-              <button className="flex gap-3 font-mono items-center border p-2 rounded-md border-[#779CBF6B]">
-                <Icons.trackicon />
-                TRACK TOKEN
-              </button>
-              <button className="flex font-mono bg-[#FF4C02] text-[#FFFFFFD4] gap-3 items-center border p-2 rounded-md border-[#779CBF6B]">
-                <Icons.autobuy />
-                SET AUTO BUY
-              </button>
-            </div>
-          </div>
-        </div>
 
-        
+          <div className="bg-[#0A141A]/60 p-3 rounded-[12px]">
+            <DialogHeader>
+              <DialogTitle className="text-[#FFFFFFD4] font-mono text-lg border-b py-3 border-brandgray">
+                {dummyTokenData.name}{" "}
+                <span className="">({dummyTokenData.symbol})</span>
+              </DialogTitle>
+            </DialogHeader>
+            <div
+              className=" w-full my-2  backdrop-blur-lg 
+        grid grid-cols-3 gap-4  text-sm"
+            >
+              {tokenInfoData.map((token) => (
+                <div
+                  key={token.name}
+                  className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]"
+                >
+                  <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                    {token.name}
+                  </span>
+                  <p className="font-mono text-[#FFFFFF5C] text-md break-all">
+                    {token.value}
+                  </p>
+                </div>
+              ))}
+              <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
+                <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                  RUG CHECK
+                </span>
+                <div className="bg-[#FF000021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FF0000] p-2 rounded-[10px]">
+                  <Icons.warningIcon className="size-[20px] text-[#FF0000] " />
+                  <p className="text-[#FFFFFF73]">Warning</p>
+                </div>
+              </div>
+              <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
+                <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                  BUY/SELL RATIO:
+                </span>
+                <div className="font-mono justify-between flex items-center  text-[13px] w-full  text-[#FFFFFF5C] text-md ">
+                  <p className="text-green-400 w-full">20% BUYS / 80% SELLS</p>
+                </div>
+              </div>
+              <div className="flex  w-full bg-[#0D1920] flex-col gap-2 px-3 py-3 rounded-[6px]">
+                <span className="text-[#FFFFFFD4]  uppercase text-xs w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                  BUY/SELL RATIO:
+                </span>
+                <div className="bg-[#FFBA0021] flex max-w-[120px] gap-3 text-[#FFFFFF73] border border-[#FFBA00] p-2 rounded-[10px]">
+                  <Icons.warningIcon className="size-[20px] text-[#FFBA00] " />
+                  Warning
+                </div>
+              </div>
+            </div>
+            <div className="h-[1px] w-full bg-brandgray my-3 "></div>
+            <div className="flex justify-between items-center">
+              <div className="flex font-mono text-sm text-white  gap-2">
+                <ChevronDown />
+                <p>VIEW DETAILS</p>
+              </div>
+              <div className="flex gap-4 ">
+                <button className="flex gap-3 font-mono items-center border p-2 rounded-md border-[#779CBF6B]">
+                  <Icons.trackicon />
+                  TRACK TOKEN
+                </button>
+                <button className="flex font-mono bg-[#FF4C02] text-[#FFFFFFD4] gap-3 items-center border p-2 rounded-md border-[#779CBF6B]">
+                  <Icons.autobuy />
+                  SET AUTO BUY
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
       </DialogContent>
     </Dialog>
   );
