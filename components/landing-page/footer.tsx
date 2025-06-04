175e1a65ff28a84b653577f6f510aa8acbcb3e2a
shittu adewale <sheriffhardewale@gmail.com>
2025-06-04 15:59:48 +0100

    feat: update Footer component layout for improved alignment and styling

--- components/landing-page/footer.tsx
+++ components/landing-page/footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Icons } from '../shared/icons'
 
 const Footer = () => {
   return (
     <footer className='bg-[#00090F] py-16'>
-        <div className='mx-auto w-[90%] md:w-[80%]'>
+        <div className='mx-auto flex items-center justify-between w-[90%] md:w-[80%]'>
+<Icons.logo className='w-20 h-20 mb-4' />
+<p className='text-[#FFFFFF70] text-2xl'>hello@socialsniper.ai</p>
 
         </div>
     </footer>
