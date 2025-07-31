325b403f27008ae5c4b8adc70046a48fdd304429
Adewale shittu <sheriffhardewale@gmail.com>
2025-07-31 17:43:21 +0100

    fix: adjust layout in DashboardSidebar for improved spacing and structure

--- app/components/shared/dashboard-sidebar.tsx
+++ app/components/shared/dashboard-sidebar.tsx
@@ -9,14 +9,14 @@ const DashboardSidebar = () => {
     const pathname = usePathname()
 
   return (
-    <section className="border-r flex flex-col justify-between w-full  pl-6 max-w-[338px] border-brandgray  ">
+    <section className="border-r flex flex-col  w-full  pl-6 max-w-[338px] border-brandgray  ">
       <div className="border-b flex justify-start items-start pt-8 pb-3 border-brandgray">
         <p className="font-pixelify text-white text-[34.85px] max-w-[117px] leading-[24.89px]">
           SOCIAL SNIPER
         </p>
       </div>
 
-      <div className=" mx-3   flex flex-col space-y-4">
+      <div className=" mx-3  h-[80vh] mt-20 flex flex-col space-y-4">
         {dashboardLinks.map((navlink) => (
           <Link
             className={
@@ -38,7 +38,7 @@ const DashboardSidebar = () => {
         ))}
       </div>
 
-      <div className="border-brandgray flex gap-3 mb-[5.24rem]  border-b py-2 w-full">
+      <div className="border-brandgray  flex gap-3 mb-[5.24rem]  border-b py-2 w-full">
         <Icons.logoutIcon />
         <p className="text-[#FF4C02] text-[19.89px] font-pixelify ">LOGOUT</p>
       </div>
